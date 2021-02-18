b3ab614c0d4444675da51cb90d9ce164b6b007c2
Javier Quinteros <javieratnora@gmail.com>
2021-02-17 23:33:39 -0300

    React Bootrsap added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import {Container, Row, Column, Col} from 'react-bootstrap'
 
 
 
@@ -8,6 +9,12 @@ const layoutStyle=
   fontFamily: "Poppins, sans-serif",
   color: "#141414",
 }
+
+const navBar=
+{
+  padding: "10%"
+}
+
 
 const mainBox ={
   paddingTop: "10%",
@@ -35,7 +42,31 @@ const IndexPage = () => {
   return (     
       <main style={layoutStyle}>
             <title>Desarrollador Fullstack - Santiago, Chile.</title>
-
+            <Container>
+              <Row >
+                <Col sm={6}>Inicio</Col>
+                <Col sm={6}>Blog</Col>
+              </Row>
+          </Container>
+            {/* <Grid container
+                  direction="column"
+                  justify="flex-start"
+                  alignItems="center"
+                  spacing={10}
+                  style={mainBox}>
+              <Grid container
+              
+              style={mainTitle}    
+              >
+                <h1>Primero que nada, buenos días<span style={fancyFont} >.</span></h1>    
+              </Grid>
+              <Grid container
+              style={secondaryTitle}    
+              >
+                <h3>Buscabas un Desarrollador Fullstack?</h3>    
+              </Grid>
+            </Grid> */}
+            
             
           </main>
    
