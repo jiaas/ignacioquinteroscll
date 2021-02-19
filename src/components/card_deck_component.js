a3c39dd470d62f8974719c2da4acd0cd42e4193a
Javier Quinteros <javieratnora@gmail.com>
2021-02-19 13:02:47 -0300

    CardDeck component fixed

--- src/components/card_deck_component.js
+++ src/components/card_deck_component.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Card, CardDeck, Button } from "react-bootstrap";
+import { Card, CardDeck } from "react-bootstrap";
 
 const CardDeckComponent = () => {
   return (
     <CardDeck>
       <Card>
-        <Card.Img variant="top" src="./assets/images/placeholder.svg" />
+        <Card.Img variant="top" src="" />
         <Card.Body>
           <Card.Title style={{ fontWeight: "bold" }}>
             Una historia: ¿Por qué construí este sitio web?
@@ -15,13 +15,6 @@ const CardDeckComponent = () => {
             bulk of the card's content.
           </Card.Text>
         </Card.Body>
-        <Card.Footer>
-          <center>
-            <Button variant="dark" size="lg" block>
-              Te invito a leer este post
-            </Button>
-          </center>
-        </Card.Footer>
       </Card>
       <Card>
         <Card.Img variant="top" src="holder.js/100px160" />
@@ -34,32 +27,18 @@ const CardDeckComponent = () => {
             bulk of the card's content.
           </Card.Text>
         </Card.Body>
-        <Card.Footer>
-          <center>
-            <Button variant="dark" size="lg" block>
-              Te invito a leer este post
-            </Button>
-          </center>
-        </Card.Footer>
       </Card>
       <Card>
         <Card.Img variant="top" src="holder.js/100px160" />
         <Card.Body>
           <Card.Title style={{ fontWeight: "bold" }}>
-            Lo que aprendí: El arte de aprender a aprender.
+            Un pensamiento: El arte de aprender a aprender.
           </Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </Card.Text>
         </Card.Body>
-        <Card.Footer>
-          <center>
-            <Button variant="dark" size="lg" block>
-              Te invito a leer este post
-            </Button>
-          </center>
-        </Card.Footer>
       </Card>
     </CardDeck>
   );
